920f9f8db005c8d3b6c6efe0f13a1e605184cd4e
Sathin Thenuja <sathinthenuja@gmail.com>
2023-10-13 20:22:35 +0530

    scoring updated

--- js/Game.js
+++ js/Game.js
@@ -11,6 +11,7 @@ const bulletHeight = 10;
 const enemyWidth = 50;
 const enemyHeight = 50;
 let isGameOver = false;
+let score = 0;
 
 const tank = {
     x: canvasWidth / 2 - tankWidth / 2,
@@ -75,14 +76,11 @@ function update() {
     } else {
         let opacity = 0;
         let animationInterval = setInterval(function () {
-            // Clear the canvas
             ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-            // Draw the fading overlay
             ctx.fillStyle = "rgba(0, 0, 0, " + opacity + ")";
             ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-            // Display the "Game Over" message and "Camp Destroyed!" with fading effect
             ctx.fillStyle = "white";
             ctx.font = "48px Arial";
             ctx.fillText("Game Over", canvasWidth / 2 - 100, canvasHeight / 2 - 24);
@@ -91,74 +89,81 @@ function update() {
             ctx.font = "52px Arial";
             ctx.fillText("Camp Destroyed!", canvasWidth / 2 - 100, canvasHeight / 2 + 24);
 
+            // Display the final score
+            ctx.fillStyle = "white";
+            ctx.font = "36px Arial";
+            ctx.fillText("Score: " + score, canvasWidth / 2 - 70, canvasHeight / 2 + 80);
+
             if (opacity < 0.7) {
                 opacity += 0.01;
             } else {
                 clearInterval(animationInterval);
-                // Add code for other game over actions or user interaction
-                // For example, a restart button, displaying score, etc.
             }
         }, 30);
     }
 
+    ctx.fillStyle = "white";
+    ctx.font = "24px Arial";
+    ctx.fillText("Score: " + score, 20, 30);
+}
 
-    function drawTank() {
-        ctx.drawImage(tank.image, tank.x, tank.y, tankWidth, tankHeight);
-    }
+function drawTank() {
+    ctx.drawImage(tank.image, tank.x, tank.y, tankWidth, tankHeight);
+}
 
-    function drawBullets() {
-        ctx.fillStyle = "red";
-        bullets.forEach(bullet => {
-            ctx.fillRect(bullet.x, bullet.y, bulletWidth, bulletHeight);
-        });
-    }
+function drawBullets() {
+    ctx.fillStyle = "red";
+    bullets.forEach(bullet => {
+        ctx.fillRect(bullet.x, bullet.y, bulletWidth, bulletHeight);
+    });
+}
 
-    function drawEnemies() {
-        ctx.fillStyle = "blue";
-        enemies.forEach(enemy => {
-            ctx.fillRect(enemy.x, enemy.y, enemyWidth, enemyHeight);
-        });
-    }
+function drawEnemies() {
+    ctx.fillStyle = "blue";
+    enemies.forEach(enemy => {
+        ctx.fillRect(enemy.x, enemy.y, enemyWidth, enemyHeight);
+    });
+}
 
-    function moveBullets() {
-        bullets.forEach((bullet, index) => {
-            bullet.y -= bullet.speed;
-            if (bullet.y < 0) {
-                bullets.splice(index, 1);
+function moveBullets() {
+    bullets.forEach((bullet, index) => {
+        bullet.y -= bullet.speed;
+        if (bullet.y < 0) {
+            bullets.splice(index, 1);
+        }
+    });
+}
+
+function moveEnemies() {
+    enemies.forEach((enemy, index) => {
+        enemy.y += enemy.speed;
+        if (enemy.y > canvasHeight) {
+            enemies.splice(index, 1);
+        }
+    });
+}
+
+function detectCollisions() {
+    bullets.forEach((bullet, bulletIndex) => {
+        enemies.forEach((enemy, enemyIndex) => {
+            if (
+                bullet.x < enemy.x + enemyWidth &&
+                bullet.x + bulletWidth > enemy.x &&
+                bullet.y < enemy.y + enemyHeight &&
+                bullet.y + bulletHeight > enemy.y
+            ) {
+                bullets.splice(bulletIndex, 1);
+                enemies.splice(enemyIndex, 1);
+                score += 10;
             }
         });
-    }
+    });
 
-    function moveEnemies() {
-        enemies.forEach((enemy, index) => {
-            enemy.y += enemy.speed;
-            if (enemy.y > canvasHeight) {
-                enemies.splice(index, 1);
-            }
-        });
-    }
-
-    function detectCollisions() {
-        bullets.forEach((bullet, bulletIndex) => {
-            enemies.forEach((enemy, enemyIndex) => {
-                if (
-                    bullet.x < enemy.x + enemyWidth &&
-                    bullet.x + bulletWidth > enemy.x &&
-                    bullet.y < enemy.y + enemyHeight &&
-                    bullet.y + bulletHeight > enemy.y
-                ) {
-                    bullets.splice(bulletIndex, 1);
-                    enemies.splice(enemyIndex, 1);
-                }
-            });
-        });
-
-        for (let i = enemies.length - 1; i >= 0; i--) {
-            const enemy = enemies[i];
-            if (enemy.y + enemyHeight > canvasHeight) {
-                isGameOver = true;
-                break;
-            }
+    for (let i = enemies.length - 1; i >= 0; i--) {
+        const enemy = enemies[i];
+        if (enemy.y + enemyHeight > canvasHeight) {
+            isGameOver = true;
+            break;
         }
     }
 }
